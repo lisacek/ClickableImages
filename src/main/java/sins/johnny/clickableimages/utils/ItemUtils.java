520f41df5e04b975d82f0fd541c9e259c4413113
Vladimir-Urik <gggedrvideos@gmail.com>
2022-08-10 16:52:55 +0200

    Add new map utility

--- src/main/java/sins/johnny/clickableimages/utils/ItemUtils.java
+++ src/main/java/sins/johnny/clickableimages/utils/ItemUtils.java
@@ -1,5 +1,6 @@
 package sins.johnny.clickableimages.utils;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.MapMeta;
@@ -8,6 +9,15 @@ public class ItemUtils {
 
     public static short getMapIdFromItemStack(final ItemStack item) {
         return item.getDurability();
+    }
+
+    public static ItemStack createMapItem(final short mapId) {
+        final ItemStack item = new ItemStack(Material.MAP, 1, mapId);
+        final ItemMeta meta = item.getItemMeta();
+        final MapMeta mapMeta = (MapMeta) meta;
+        mapMeta.setScaling(false);
+        item.setItemMeta(mapMeta);
+        return item;
     }
 
 }
